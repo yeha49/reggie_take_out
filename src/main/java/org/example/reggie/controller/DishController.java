8128ef4c498fb5d95fde3918b2d97e53e65d2c7a
yeha49 <yeha.tw@yahoo.com.tw>
2022-06-16 15:21:33 +0800

    使用redis緩存短訊驗證碼及菜品數據

--- src/main/java/org/example/reggie/controller/DishController.java
+++ src/main/java/org/example/reggie/controller/DishController.java
@@ -13,9 +13,12 @@ import org.example.reggie.service.DishFlavorService;
 import org.example.reggie.service.DishService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RestController
@@ -28,10 +31,20 @@ public class DishController {
     private DishFlavorService dishFlavorService;
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping
     public R<String> save(@RequestBody DishDto dishDto) {
         log.info(dishDto.toString());
+
+        //清理所有菜品緩存數據
+//        Set keys = redisTemplate.keys("dish_*");
+//        redisTemplate.delete(keys);
+
+        //只清理特定菜品
+        String key = "dish_" + dishDto.getCategoryId() + "_1";
+        redisTemplate.delete(key);
 
         dishService.saveWithFlavor(dishDto);
 
@@ -88,10 +101,18 @@ public class DishController {
     }
 
     @PutMapping
-    public R<String> update(DishDto dishDto) {
-        log.info(dishDto.toString());
+    public R<String> update(@RequestBody DishDto dishDto) {
+        log.info("dishDto:"+dishDto.toString());
 
         dishService.updateWithFlavor(dishDto);
+
+        //清理所有菜品緩存數據
+//        Set keys = redisTemplate.keys("dish_*");
+//        redisTemplate.delete(keys);
+
+        //只清理特定菜品
+        String key = "dish_" + dishDto.getCategoryId() + "_1";
+        redisTemplate.delete(key);
 
         return R.success("修改菜品成功");
     }
@@ -112,6 +133,17 @@ public class DishController {
 
     @GetMapping("/list")
     public R<List<DishDto>> list( Dish dish){
+        //先從redis查數據
+        List<DishDto> dishDtoList = null;
+        String key = "dish_" + dish.getCategoryId() + "_" + dish.getStatus();
+        dishDtoList = (List<DishDto>) redisTemplate.opsForValue().get(key);
+
+        if (dishDtoList !=null){
+        //如果有，直接返回
+            return R.success(dishDtoList);
+        }
+        //如果沒有，就查mysql，再把數據緩存到redis
+
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
         queryWrapper.eq(Dish::getStatus, 1);
@@ -121,7 +153,7 @@ public class DishController {
 
         List<Dish> list = dishService.list(queryWrapper);
 
-        List<DishDto> dishDtoList = list.stream().map((item)->{
+        dishDtoList = list.stream().map((item)->{
             DishDto dishDto = new DishDto();
 
             BeanUtils.copyProperties(item, dishDto);
@@ -142,7 +174,8 @@ public class DishController {
 
             return dishDto;
         }).collect(Collectors.toList());
-
+        //如果沒有，就查mysql，再把數據緩存到redis
+        redisTemplate.opsForValue().set(key, dishDtoList, 60, TimeUnit.MINUTES);
 
         return R.success(dishDtoList);
     }
--- src/main/java/org/example/reggie/controller/UserController.java
+++ src/main/java/org/example/reggie/controller/UserController.java
@@ -10,6 +10,7 @@ import org.example.reggie.service.UserService;
 import org.example.reggie.utils.SMSUtils;
 import org.example.reggie.utils.ValidateCodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -24,6 +26,9 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user, HttpSession session){
         String phone = user.getPhone();
@@ -33,7 +38,10 @@ public class UserController {
             log.info("code {}", code);
 //            SMSUtils.sendMessage("reggie_take_out", "", phone, code);
 
-            session.setAttribute(phone,code);
+//            session.setAttribute(phone,code);
+
+            //驗證碼改存到redis，期限5分鐘
+            redisTemplate.opsForValue().set(phone, code, 5, TimeUnit.MINUTES);
 
             return R.success("手機驗證碼發送成功");
         }
@@ -47,8 +55,9 @@ public class UserController {
 
         String code = map.get("code").toString();
 
-        Object codeInSession = session.getAttribute(phone);
-
+//        Object codeInSession = session.getAttribute(phone);
+        //改從redis讀取驗證碼
+        Object codeInSession = redisTemplate.opsForValue().get(phone);
 
 
         if (codeInSession != null && codeInSession.equals(code)) {
@@ -63,6 +72,9 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user", user.getId());
+            //如果用戶登陸成功，就可以刪除redis中的驗證碼
+            redisTemplate.delete(phone);
+
             return R.success(user);
         }
         return R.error("登陸失敗");
